0801b8c098ffbaa22ae6c8184dbdb4ef15232b94
parest <pares.thibault@outlook.com>
2021-12-19 13:53:00 +0100

    hero interface & hero details

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  hero: Hero = {
+    id: 1,
+    name: 'Tempête de Vent'
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
