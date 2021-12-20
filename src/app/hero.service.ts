1c4c9f1d6728ef11acf4ab877c81e020dc842ad1
parest <pares.thibault@outlook.com>
2021-12-20 11:45:55 +0100

    messageService & messageComponent OK

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  // "service in service" scénario : MessageService injecté dans HeroService injecté dans HeroesComponent
+  constructor(private messageService: MessageService) { }
 
   // méthode asyncrone: l'application doit attendre que getHeroes ait récupéré les données depuis un serveur (le cas échéant)
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+    this.messageService.add("HeroService: fetched heroes");
     return heroes;
   }
 }
