f2c43e9668fd114d0cbbade8ad2da1eeecfdb321
parest <pares.thibault@outlook.com>
2021-12-20 18:07:51 +0100

    web api added

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
@@ -10,8 +11,12 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
 
+  private heroesUrl = 'api/heroes'; // url de l'api web
+
   // "service in service" scénario : MessageService injecté dans HeroService injecté dans HeroesComponent
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient) { }
 
   // méthode asyncrone: l'application doit attendre que getHeroes ait récupéré les données depuis un serveur (le cas échéant)
   getHeroes(): Observable<Hero[]> {
@@ -24,5 +29,10 @@ export class HeroService {
     const hero = HEROES.find(h => h.id === id)!;
     this.messageService.add("HeroService: fetched hero " + hero.id);
     return of(hero);
+  }
+
+  // Log a HeroService message with the MessageService
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
