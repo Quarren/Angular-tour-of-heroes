66f8ccc90df4492d0e5dd531e8ab19cdb965a13c
parest <pares.thibault@outlook.com>
2021-12-20 11:55:15 +0100

    messages service & component OK

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -14,7 +16,9 @@ export class HeroesComponent implements OnInit {
 
   // bonnes pratiques : "Reserve the constructor for minimal initialization such as wiring constructor parameters to properties"
   // -> le constructeur ne doit rien faire (pas d'appel de méthodes)
-  constructor(private heroService: HeroService) { }
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -22,6 +26,7 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero) {
     this.selectedHero = hero;
+    this.messageService.add("HeroesComponent: Selected hero id " + this.selectedHero.id);
   };
 
   // la méthode attend que l' Observable<Hero[]> émette le tableau de Hero
